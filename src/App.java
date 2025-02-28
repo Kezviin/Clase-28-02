bc8732b92240f326d8161fc96c95096bfe1b3954
kezvin <Kevinlibardorosas@gmail.com>
2025-02-28 13:19:08 -0500

    Se realiza el primer ejecicio

--- src/App.java
+++ src/App.java
@@ -1,5 +1,20 @@
+import java.util.Scanner;
+
+import javax.print.DocFlavor.STRING;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        System.out.println("ingresa el nombre de usuario");
+        Scanner scanner = new Scanner(System.in);
+        String nombre = scanner.nextLine();
+        System.out.println("Ingresa la edad del usuario");
+        byte edad = scanner.nextByte();
+        System.out.println("Escriba la altura del usuario en cm");
+        byte altura = scanner.nextByte();
+        System.out.println("Se llama: " + nombre + " Con edad: " + edad + " Y altura : " + altura);
+
+
+
+
     }
 }
